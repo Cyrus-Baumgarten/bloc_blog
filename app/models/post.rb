d21b84cdb8be481f945b9d1647705c419cab5ae1
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-17 06:28:14 -0700

    finishes preliminary validations with updated models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :author_id, :body, :comments, :title
   
-  validates :body, presence: true
-  validates :title, presence: true, uniqueness: true
+  validates :body,    presence: true
+  validates :title,   presence: true, uniqueness: true
   
   belongs_to :author
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :role, :anonymous, :email, :image_url, :name, :password, :password_confirm, :subscribe
     
+  validates :name,              presence: true, uniqueness: true
+  validates :password,          presence: true
+  validates :password_confirm,  presence: true
+  validates :email,             presence: true, uniqueness: true
+  
+  
   
   has_many :comments
 end
