c4a9044f40225e391e2423c6ebc182bf6ba3cfd0
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-17 05:29:05 -0700

    adds user sign up link, form for new user, actions for creating and showing new user, flash with user sign up success, sign up redirects to users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,17 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
   
   def create
+    @user = User.create(params[:user])
+    flash[:welcome] = "Welcome new user"
+    redirect_to @user
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
   end
   
 end
