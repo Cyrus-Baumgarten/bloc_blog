8719d373e6dca5b73791c691e84cac17edbc5b36
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-17 06:01:28 -0700

    adds preliminary validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   attr_accessible :author_id, :content, :post_id, :user_id
   
+  validates :content, presence: true, length: { maximum: 160 }
+  
   belongs_to :author
   belongs_to :user
   belongs_to :post
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :admin, :anonymous, :email, :image_url, :name, :password, :subscribe
   
+  validates :title, presence: true, uniqueness: true
+  
+  
+  
   has_many :comments
 end
