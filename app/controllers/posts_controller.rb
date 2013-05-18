7a2f8378e50dfb1535fd60f5861ad87dad63ca6e
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-18 03:38:22 -0700

    updating saves

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_id(params[:id])
     @comments = Comment.all
-    @comment = @post.comments.new
+    @comment = Comment.new
   end
   
   def new
