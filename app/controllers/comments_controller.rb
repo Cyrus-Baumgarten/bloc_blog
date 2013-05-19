e5d8bab46748e311f91ccedcca8604b932f039cf
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-19 03:08:23 -0700

    adds functional delete links for comments, edit links do not work, glitch where there's an extra set of links at bottom of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,19 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(params[:comment])
     redirect_to :back
   end
+  
+  def edit
+    @comment = Comment.find_by_id(params[:id])
+  end
+  
+  def update
+    @comment = Comment.find_by_id(params[:id])
+    @comment.update_attributes(params[:comment])
+    redirect_to @post
+  end
+  
+  def destroy
+    Comment.find_by_id(params[:id]).destroy
+    redirect_to :back
+  end
 end
