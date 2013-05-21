bafb405e9bbd3722a399098f9010f8ade1ef60ed
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-21 01:41:41 -0700

    when signed in, adds notice with current commening behavior and toggle button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-    
+  
+  def toggle
+    if current_user.anonymous?
+      current_user.update_attributes(anonymous: false)
+      flash[:success] = "Your comments are now credited to your username"
+      redirect_to :back
+    elsif current_user.name.empty?
+      current_user.update_attributes(anonymous: false)
+      flash[:error] = "You need a user name if you want your comments to be credited"
+      redirect_to edit_user_path(current_user)
+    else
+      current_user.update_attributes(anonymous: true)
+      flash[:success] = "Your comments and profile are now anonymous"
+      redirect_to :back
+    end
+  end
+  
+  
   def show
     @user = User.find(params[:id])
   end
