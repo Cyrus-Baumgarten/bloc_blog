3d2a1727cfc22bee261fb884d93bf0e4c3f8da23
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-21 06:09:30 -0700

    adds first (failing) user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(
+    name: "tester",
+    email: "tester@test.com",
+    password: "foobar",
+    password_confirmation: "foobar",
+    remember_me: true,
+    anonymous: false,
+    image_url: "www.image.com",
+    subscribe: false)
+  end
+
+  it { should be_valid }
+
+end
