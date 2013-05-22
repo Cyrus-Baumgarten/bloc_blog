9071b69b1d2c9b2f6a93970536ff51c1e0d96dd7
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-21 22:38:09 -0700

    initial user test green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 describe User do
+  
 
   before do
     @user = User.new(
-    name: "tester",
-    email: "tester@test.com",
-    password: "foobar",
-    password_confirmation: "foobar",
-    remember_me: true,
-    anonymous: false,
-    image_url: "www.image.com",
-    subscribe: false)
+      name: "tester",
+      email: "tester@test.com",
+      password: "password",
+      password_confirmation: "password",
+      remember_me: true,
+      anonymous: false,
+      image_url: "www.image.com",
+      subscribe: false )
   end
+  
+  subject { @user }
 
   it { should be_valid }
+  it { should respond_to(:admin) }
+  it { should respond_to(:email) }
+  
+  
+  
 
 end
