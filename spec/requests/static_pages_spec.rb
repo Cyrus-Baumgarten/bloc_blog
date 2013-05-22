79f811ae8c325e92e3628ca8c342e9035b883807
Cyrus-Baumgarten <cdbaumg@gmail.com>
2013-05-22 15:48:10 -0700

    register test green, sign out test fail

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe "Pages" do
   
-  describe "register" do
-  
+  describe "register with valid information" do
     before do
       visit new_user_registration_path
       fill_in "Email", with: "user@example.com"
@@ -11,12 +10,24 @@ describe "Pages" do
       fill_in "Password confirmation", with: "password"
       click_button "Sign up"
     end
-
     describe "it should create and sign in the user" do
       subject { page }
-      it { should have_content('') }
+      it { should have_content('Welcome! You have signed up successfully.') }
     end
-    
+  end
+
+  describe "sign out" do
+    before do
+      visit new_user_registration_path
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
+      visit destroy_user_session_path
+    end
+    subject { page }
+    it { should have_content('Signed out successfully.') }
   end
 
 end
+
